da630bd3f75fabf2470637263dea313743da6c6c
Lixin Huang <lixinhenryhuang@berkeley.edu>
2024-01-31 14:51:28 -0800

    init

---
+++ parameters/monomer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 16:59:14 2020
+
+@author: LiwenKo
+"""
+
+from numpy import array
+
+# in Debye
+dipoles = array([[4, 0, 0]]) 
+
+# in cm^-1
+H_sys = array([15222])
+
+lambdas = array([37.])
+
+gammas = array([30.])
